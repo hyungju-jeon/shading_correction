8c77fd082a8ce9950de79c67816e763621ee9a9e
hyungju jeon <hyungju.jeon@gmail.com>
2022-08-26 17:32:00 +0900

    cli updated

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+from src.run_shading_correction import *
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -7,10 +8,10 @@ def cli():
     pass
 
 @cli.command(help="Corrects unstitched 3D stacks in LSM or TIFF")
-@click.option('--input', '-i', default='', help='Input file path')
 @click.argument('input_path')
-def stack(input_path, input):
-    click.echo("Hello, {}".format(input))
+@click.option('--output_path', '-o', default='', help='Output file path')
+def stack(input_path, output_path):
+    stack_shading_correction(input_path, output_path)
 
 @cli.command(help="Corrects stitched 2D image in CZI")
 @click.option('--input', '-i', default='', help='Input file path')
